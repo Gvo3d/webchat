7dc9d7d56227c77b93c713b6f283f221526747a0
Gvo3d <denis.yakimov89@gmail.com>
2016-07-26 18:35:41 +0300

    v.0.0.0.2

--- src/main/java/Servlets/IndexServlet.java
+++ src/main/java/Servlets/IndexServlet.java
@@ -1,10 +1,33 @@
 package Servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Gvozd on 26.07.2016.
  */
 public class IndexServlet extends HttpServlet {
-    
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        HttpSession session = req.getSession();
+        session.setMaxInactiveInterval(5);
+
+
+        out.println("Session interval is "+ session.getMaxInactiveInterval());
+
+        Integer ival = (Integer) session.getAttribute("sesspeek.cntr");
+        if (ival == null)
+            ival = new Integer(1);
+        else
+            ival = new Integer(ival.intValue() + 1);
+        session.setAttribute("sesspeek.cntr", ival);
+        out.println("You have hit this page " + ival + " times.<p>");
+        out.close();
+    }
 }
